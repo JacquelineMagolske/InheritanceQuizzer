eb7a67d39b54bc4f5c723d9c79982be5d2db85db
jacquelinemagolske <jacquelinemagolske@localhost>
2025-02-04 00:07:21 -0700

    Second commit

--- Inheritance Quiz/src/inheritanceQuiz.java
+++ Inheritance Quiz/src/inheritanceQuiz.java
@@ -8,11 +8,21 @@ public class inheritanceQuiz
 		
 		public static void main(String[] args)
 			{
-				
-				
+			
 				questionOne();
+				questionTwo();
+				questionThree();
+				questionFour();
+				questionFive();
+				questionSix();
+				questionSeven();
+				questionEight();
+				questionNine();
+				questionTen();
+				questionEleven();
+				questionTwelve();
 				
-				return userScore();
+				System.out.println("Your final score is " + userScore + " out of 12.");
 				
 
 			}
@@ -28,12 +38,13 @@ public class inheritanceQuiz
 			System.out.println("B. I know everything about them");
 			System.out.println("C. Hold on tight spider monkey");
 			System.out.println("D. Did you know Robert Pattinson is getting married to Suki Waterhouse instead of me????");
+			System.out.println("E. This code will not execute");
 			
 			Scanner userInput = new Scanner (System.in);
 			
 			String answer = userInput.nextLine();
 			
-			if(answer == "A") 
+			if(answer.equals("A")) 
 			{
 				userScore++;
 			}
@@ -42,6 +53,292 @@ public class inheritanceQuiz
 		}
 		
 		
+		public static void questionTwo()
+		{
+			System.out.println("If the code runs: ");
+			System.out.println("		CrazyPerson x = new CrazyPerson();");
+			System.out.println("		x.runCrazyPersonMethod2()");
+			System.out.println("What will the result be?");
 			
+			System.out.println("A. I LOVE LOVE LOVE LOVE LOVE AHHH");
+			System.out.println("B. I know everything about them");
+			System.out.println("C. Hold on tight spider monkey");
+			System.out.println("D. Did you know Robert Pattinson is getting married to Suki Waterhouse instead of me????");
+			System.out.println("E. This code will not execute");
+			
+			Scanner userInput = new Scanner (System.in);
+			
+			String answer = userInput.nextLine();
+			
+			if(answer.equals("B")) 
+			{
+				userScore++;
+			}
+			
+			
+		}
+			
+		
+		public static void questionThree()
+		{
+			System.out.println("If the code runs: ");
+			System.out.println("		CrazyPerson x = new CrazyPerson();");
+			System.out.println("		x.runTwilightFanMethod1()");
+			System.out.println("What will the result be?");
+			
+			System.out.println("A. I LOVE LOVE LOVE LOVE LOVE AHHH");
+			System.out.println("B. I know everything about them");
+			System.out.println("C. Hold on tight spider monkey");
+			System.out.println("D. Did you know Robert Pattinson is getting married to Suki Waterhouse instead of me????");
+			System.out.println("E. This code will not execute");
+			
+			Scanner userInput = new Scanner (System.in);
+			
+			String answer = userInput.nextLine();
+			
+			if(answer.equals("C")) 
+			{
+				userScore++;
+			}
+			
+			
+		}
+		
+		
+		public static void questionFour()
+		{
+			System.out.println("If the code runs: ");
+			System.out.println("		CrazyPerson x = new TwilightFan();");
+			System.out.println("		x.runCrazyPersonMethod1()");
+			System.out.println("What will the result be?");
+			
+			System.out.println("A. I LOVE LOVE LOVE LOVE LOVE AHHH");
+			System.out.println("B. I know everything about them");
+			System.out.println("C. Hold on tight spider monkey");
+			System.out.println("D. Did you know Robert Pattinson is getting married to Suki Waterhouse instead of me????");
+			System.out.println("E. This code will not execute");
+			
+			Scanner userInput = new Scanner (System.in);
+			
+			String answer = userInput.nextLine();
+			
+			if(answer.equals("A")) 
+			{
+				userScore++;
+			}
+			
+			
+		}
+		
+		
+		public static void questionFive()
+		{
+			System.out.println("If the code runs: ");
+			System.out.println("		CrazyPerson x = new TwilightFan();");
+			System.out.println("		x.runCrazyPersonMethod2()");
+			System.out.println("What will the result be?");
+			
+			System.out.println("A. I LOVE LOVE LOVE LOVE LOVE AHHH");
+			System.out.println("B. I know everything about them");
+			System.out.println("C. Hold on tight spider monkey");
+			System.out.println("D. Did you know Robert Pattinson is getting married to Suki Waterhouse instead of me????");
+			System.out.println("E. This code will not execute");
+			
+			Scanner userInput = new Scanner (System.in);
+			
+			String answer = userInput.nextLine();
+			
+			if(answer.equals("B")) 
+			{
+				userScore++;
+			}
+			
+			
+		}
+		
+		
+		public static void questionSix()
+		{
+			System.out.println("If the code runs: ");
+			System.out.println("		CrazyPerson x = new TwilightFan();");
+			System.out.println("		x.runTwilightFanMethod1()");
+			System.out.println("What will the result be?");
+			
+			System.out.println("A. I LOVE LOVE LOVE LOVE LOVE AHHH");
+			System.out.println("B. I know everything about them");
+			System.out.println("C. Hold on tight spider monkey");
+			System.out.println("D. Did you know Robert Pattinson is getting married to Suki Waterhouse instead of me????");
+			System.out.println("E. This code will not execute");
+			
+			Scanner userInput = new Scanner (System.in);
+			
+			String answer = userInput.nextLine();
+			
+			if(answer.equals("C")) 
+			{
+				userScore++;
+			}
+			
+			
+		}
+		
+		
+		public static void questionSeven()
+		{
+			System.out.println("If the code runs: ");
+			System.out.println("		TwilightFan x = new CrazyPerson();");
+			System.out.println("		x.runCrazyPersonMethod1()");
+			System.out.println("What will the result be?");
+			
+			System.out.println("A. I LOVE LOVE LOVE LOVE LOVE AHHH");
+			System.out.println("B. I know everything about them");
+			System.out.println("C. Hold on tight spider monkey");
+			System.out.println("D. Did you know Robert Pattinson is getting married to Suki Waterhouse instead of me????");
+			System.out.println("E. This code will not execute");
+			
+			Scanner userInput = new Scanner (System.in);
+			
+			String answer = userInput.nextLine();
+			
+			if(answer.equals("A")) 
+			{
+				userScore++;
+			}
+			
+			
+		}
+		
+		
+		public static void questionEight()
+		{
+			System.out.println("If the code runs: ");
+			System.out.println("		TwilightFan x = new CrazyPerson();");
+			System.out.println("		x.runCrazyPersonMethod2()");
+			System.out.println("What will the result be?");
+			
+			System.out.println("A. I LOVE LOVE LOVE LOVE LOVE AHHH");
+			System.out.println("B. I know everything about them");
+			System.out.println("C. Hold on tight spider monkey");
+			System.out.println("D. Did you know Robert Pattinson is getting married to Suki Waterhouse instead of me????");
+			System.out.println("E. This code will not execute");
+			
+			Scanner userInput = new Scanner (System.in);
+			
+			String answer = userInput.nextLine();
+			
+			if(answer.equals("D")) 
+			{
+				userScore++;
+			}
+			
+			
+		}
+		
+		
+		public static void questionNine()
+		{
+			System.out.println("If the code runs: ");
+			System.out.println("		TwilightFan x = new CrazyPerson();");
+			System.out.println("		x.runTwilightFanMethod1()");
+			System.out.println("What will the result be?");
+			
+			System.out.println("A. I LOVE LOVE LOVE LOVE LOVE AHHH");
+			System.out.println("B. I know everything about them");
+			System.out.println("C. Hold on tight spider monkey");
+			System.out.println("D. Did you know Robert Pattinson is getting married to Suki Waterhouse instead of me????");
+			System.out.println("E. This code will not execute");
+			
+			Scanner userInput = new Scanner (System.in);
+			
+			String answer = userInput.nextLine();
+			
+			if(answer.equals("E")) 
+			{
+				userScore++;
+			}
+			
+			
+		}
+		
+		
+		public static void questionTen()
+		{
+			System.out.println("If the code runs: ");
+			System.out.println("		TwilightFan x = new TwilightFan();");
+			System.out.println("		x.runCrazyPersonMethod1()");
+			System.out.println("What will the result be?");
+			
+			System.out.println("A. I LOVE LOVE LOVE LOVE LOVE AHHH");
+			System.out.println("B. I know everything about them");
+			System.out.println("C. Hold on tight spider monkey");
+			System.out.println("D. Did you know Robert Pattinson is getting married to Suki Waterhouse instead of me????");
+			System.out.println("E. This code will not execute");
+			
+			Scanner userInput = new Scanner (System.in);
+			
+			String answer = userInput.nextLine();
+			
+			if(answer.equals("E")) 
+			{
+				userScore++;
+			}
+			
+			
+		}
+		
+		
+		public static void questionEleven()
+		{
+			System.out.println("If the code runs: ");
+			System.out.println("		TwilightFan x = new TwilightFan();");
+			System.out.println("		x.runCrazyPersonMethod2()");
+			System.out.println("What will the result be?");
+			
+			System.out.println("A. I LOVE LOVE LOVE LOVE LOVE AHHH");
+			System.out.println("B. I know everything about them");
+			System.out.println("C. Hold on tight spider monkey");
+			System.out.println("D. Did you know Robert Pattinson is getting married to Suki Waterhouse instead of me????");
+			System.out.println("E. This code will not execute");
+			
+			Scanner userInput = new Scanner (System.in);
+			
+			String answer = userInput.nextLine();
+			
+			if(answer.equals("E")) 
+			{
+				userScore++;
+			}
+			
+			
+		}
+		
+		
+		public static void questionTwelve()
+		{
+			System.out.println("If the code runs: ");
+			System.out.println("		TwilightFan x = new TwilightFan();");
+			System.out.println("		x.runTwilightFanMethod1()");
+			System.out.println("What will the result be?");
+			
+			System.out.println("A. I LOVE LOVE LOVE LOVE LOVE AHHH");
+			System.out.println("B. I know everything about them");
+			System.out.println("C. Hold on tight spider monkey");
+			System.out.println("D. Did you know Robert Pattinson is getting married to Suki Waterhouse instead of me????");
+			System.out.println("E. This code will not execute");
+			
+			Scanner userInput = new Scanner (System.in);
+			
+			String answer = userInput.nextLine();
+			
+			if(answer.equals("C")) 
+			{
+				userScore++;
+			}
+			
+			
+		}
+		
+		
+		
 
 	}
